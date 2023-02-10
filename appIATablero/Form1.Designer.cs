9809f14841e139097a63437c9a1101a930cc88a5
Johan Ortiz <m.johan.ortiz@gmail.com>
2023-02-09 20:26:01 -0600

    Version 1

---
+++ appIATablero/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace appIATablero
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 479);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(145, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Generar Area";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(485, 450);
+			this.panel1.TabIndex = 2;
+			this.panel1.Click += new System.EventHandler(this.button1_Click);
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(352, 479);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(145, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Inicia";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(509, 514);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Tablero";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ appIATablero/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appIATablero
+{
+	public partial class Form1 : Form
+	{
+
+		// VARIABLES GLOBALES
+		List<RectangleF> coordenadas = new List<RectangleF>(); //GUARDARA INFORMACION DE LAS POSICIONES 
+															   //DE CUBOS GENERADOS
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+			Pen pincel = new Pen(Color.Black, 1);
+
+			PointF[] tablero =
+			{
+			new PointF(10.0F, 10.0F),
+			new PointF (350.0F, 10.0F),
+			new PointF (350.0F, 350.0F),
+			new PointF (10.0F, 350.0F),
+			new PointF(10.0F, 10.0F)
+			};
+
+			// TABLERO DIMESIONES A GENERAR OBSTACULOS
+			// x10, y10					x350, y10
+			//    ----------------------------
+			//	  .							 .
+			//	  .							 .
+			//	  .							 .
+			//	  .							 .
+			//	  .							 .
+			//    ----------------------------
+			// x10, y350				x350, y350
+
+			// GENERA CUADRICULA DE 8 x 8 
+
+			float espaciado = (350 - 10) / 8; //PIXELES ENTRE DIVISION DE AREAS 350px
+											  //-10 por el espacio que comienza a generar linea en el panel
+			generaCuadricula(10, 350, espaciado);
+			e.Graphics.DrawLines(pincel, tablero);
+			generaObstaculos(e);
+
+		}
+
+		private void generaObstaculos(PaintEventArgs e)
+		{
+			Random random = new Random();
+			int des;
+			SolidBrush paso = new SolidBrush(Color.FromArgb(50, 0, 0, 0));
+			SolidBrush obstaculo = new SolidBrush(Color.FromArgb(150, 0, 0, 0));
+
+			for (int i = 0; i < coordenadas.Count; i++)
+			{
+				des = random.Next(0, 10) + 1;
+				if (des > 3) // MAS ALTO = MAS OBSTACULOS
+				{
+					e.Graphics.FillRectangle(paso, coordenadas.ElementAt(i));
+				}
+				else
+				{
+					e.Graphics.FillRectangle(obstaculo, coordenadas.ElementAt(i));
+					//falta metadatos para obstaculos (iran para python)
+				}
+			}
+
+		}
+
+		//GENERA CUADRICULA SE ENCARGA DE RECOPILAR POSICIONES DEL DIBUJO DIVIDIENDO EL TAMAÑO DE PIXELES
+		//A PARTES PARA DIBUJAR
+		private void generaCuadricula(float valIni, float valFin, float valEspa)
+		{
+			float valPointx = valIni;
+			float valPointy = valIni;
+
+			float valSizeH = valEspa; // HEIGHT
+			float valSizeW = valEspa; // WIDTH
+
+			for (int i = 0; i < 8; i++) 
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					coordenadas.Add(new RectangleF(new PointF(valPointx, valPointy), new SizeF(valSizeW, valSizeH)));
+					valPointx += valSizeW;
+				}
+				valPointx = valIni;
+				valPointy += valSizeH;
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Refresh();  
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
